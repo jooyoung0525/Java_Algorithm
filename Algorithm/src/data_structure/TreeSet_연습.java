93b24953adc0707f646b3641051447b427ada714
jooyoung0525 <pjuy0525@naver.com>
2020-05-23 17:17:20 +0900

    treeset

---
+++ Algorithm/src/data_structure/TreeSet_연습.java
@@ -0,0 +1,15 @@
+package data_structure;
+
+import java.util.TreeSet;
+
+public class TreeSet_¿¬½À {
+	public static void main(String[] args) {
+		TreeSet<Integer> treeset = new TreeSet();
+		treeset.add(5);
+		treeset.add(3);
+		treeset.add(1);
+		for(Integer data : treeset){
+			System.out.println(data);
+		}
+	}
+}
